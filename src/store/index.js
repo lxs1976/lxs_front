5ecfd1db1dc4f14766236b3d973c7797b8d48552
lxs <1207564097@qq.com>
2022-06-27 13:08:41 +0800

    前端侧板拦,马上入驻和社区登入

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from "@/store/module/user";
+import getters from "@/store/getters";
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
     user
-  }
+  },
+  getters,
 })
 
 export default store
